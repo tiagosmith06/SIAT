d97ec6d4d6a270f57313be1247e09d596a1baafa
SANTIAGO PEREA MURILLO <santiagopereamurillo@gmail.com>
2023-07-28 00:59:29 -0500

    aa

--- resources/views/pages/biogeografico.blade.php
+++ resources/views/pages/biogeografico.blade.php
@@ -280,7 +280,7 @@
             </script>
 
     <h1 class="text-center  m-4 p-4">COMUNIDADES</h1>
-    <div class=" m-2 w-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class=" m-2 w-auto bg-green border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div class="sm:hidden">
             <label for="tabs" class="sr-only">Select tab</label>
             <select id="tabs" class="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -289,7 +289,7 @@
                 <option>FAQ</option>
             </select>
         </div>
-        <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
+        <ul class="bg-green-800 hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
             <li class="w-full">
                 <button id="stats-tab" data-tabs-target="#stats" type="button" role="tab" aria-controls="stats" aria-selected="true" class="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">TENENCIA COLECTIVA</button>
             </li>
