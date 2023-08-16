e50ae9143d8d24421f7fe5c47613602adc423bcf
SANTIAGO PEREA MURILLO <santiagopereamurillo@gmail.com>
2023-08-16 00:51:32 -0500

    sa

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container my-24 mx-auto md:px-6">
 
-        
+
         <!-- Section: Design Block -->
         <section class="mb-32">
           <style>
@@ -13,7 +13,7 @@
           </style>
           <h2 class="mb-12 text-center text-3xl font-bold">NOVEDADES</h2>
 
-          <div class="grid gap-6 lg:grid-cols-3">
+          <div class="grid gap-6 lg:grid-cols-2">
             <div
               class="zoom relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
               data-te-ripple-init
@@ -112,55 +112,8 @@
               </a>
             </div>
 
-            <div
-              class="zoom relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
-              data-te-ripple-init
-              data-te-ripple-color="light">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/028.jpg"
-                class="w-full align-middle transition duration-300 ease-linear" />
-              <a href="#!">
-                <div
-                  class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
-                  <div class="flex h-full items-end justify-start">
-                    <div class="m-6 text-white">
-                      <h5 class="mb-3 text-lg font-bold">
-                        A hut in the mountains
-                      </h5>
-                      <p>
-                        <small>Published <u>07.01.2022</u> by David Beak</small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div
-                  class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
-              </a>
-            </div>
 
-            <div
-              class="zoom relative overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
-              data-te-ripple-init
-              data-te-ripple-color="light">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/049.jpg"
-                class="w-full align-middle transition duration-300 ease-linear" />
-              <a href="#!">
-                <div
-                  class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsla(0,0%,0%,0.4)] bg-fixed">
-                  <div class="flex h-full items-end justify-start">
-                    <div class="m-6 text-white">
-                      <h5 class="mb-3 text-lg font-bold">Beautiful waterfall</h5>
-                      <p>
-                        <small>Published <u>04.01.2022</u> by Joe Svan</small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div
-                  class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out hover:bg-[hsla(0,0%,99%,0.15)]"></div>
-              </a>
-            </div>
+
           </div>
         </section>
         <!-- Section: Design Block -->
